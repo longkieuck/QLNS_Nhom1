ab8f15a13fe9df0e68d3ee5034896c8be32d4096
longkieuck <longkieuck@gmail.com>
2021-02-01 11:11:55 +0700

    merge success dat vs hien

--- QLNS_Nhom1/DataAcessLayer/DataProvider.cs
+++ QLNS_Nhom1/DataAcessLayer/DataProvider.cs
@@ -22,7 +22,7 @@ namespace QLNS_Nhom1.DataAcessLayer
         /// <summary>
         /// Long
         /// </summary>
-       // private string connectionSTR = "Data Source=.\\sqlexpress01;Initial Catalog=QLNS;Integrated Security=True";
+        private string connectionSTR = "Data Source=.\\sqlexpress01;Initial Catalog=QLNS;Integrated Security=True";
         public static DataProvider Instance
         {
             get { if (instance == null) instance = new DataProvider(); return DataProvider.instance; }
@@ -41,7 +41,7 @@ namespace QLNS_Nhom1.DataAcessLayer
         {
             DataTable data = new DataTable();
 
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlConnection connection = new SqlConnection(connectionSTR))
             {
                 connection.Open();
 
@@ -75,7 +75,7 @@ namespace QLNS_Nhom1.DataAcessLayer
         {
             int data = 0;
 
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlConnection connection = new SqlConnection(connectionSTR))
             {
                 connection.Open();
 
@@ -107,7 +107,7 @@ namespace QLNS_Nhom1.DataAcessLayer
         {
             object data = 0;
 
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlConnection connection = new SqlConnection(connectionSTR))
             {
                 connection.Open();
 
